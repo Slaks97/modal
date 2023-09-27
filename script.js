8e75667ca806d0f58ab095edb72233dcb41101a5
@Slaks97 <86403879+Slaks97@users.noreply.github.com>
2023-09-27 10:50:12 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// Variables
+const openBtn = document.getElementById("open-btn");
+const closeBtn = document.getElementById("close-btn");
+const modalContainer = document.getElementById("modal-container");
+
+// Event Listeners
+openBtn.addEventListener("click", function() {
+    modalContainer.style.display = 'block';
+});
+
+closeBtn.addEventListener("click", function() {
+    modalContainer.style.display = 'none';
+});
+
+window.addEventListener("click", function(e) {
+    if (e.target === modalContainer) {
+        modalContainer.style.display = 'none';
+    }
+});
